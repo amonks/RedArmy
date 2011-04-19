0ac0deff3ae3b27390d55d22515de31927247889
ajm <ajm@andrewmonks.net>
2011-04-19 19:07:44 -0400

    testing and about/contact pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,6 +8,10 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "redarmy.es | Home")
+    end
   end
 
   describe "GET 'contact'" do
@@ -15,12 +19,20 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => "redarmy.es | Contact")
+    end
   end
   
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => "redarmy.es | About")
     end
   end
 
