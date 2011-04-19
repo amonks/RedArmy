d0bbc094b808f085308d932f39a1c0981a9ccb6b
ajm <ajm@andrewmonks.net>
2011-04-19 16:00:55 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redarmy::Application.config.secret_token = 'e7aa79b5231b1c8b90ada7baa66968deb90afa93e656ddeb7f2f1ce3f8c4424afad2901570b73849557a7950cb92614b01ed3f2e8f4bbc76f588e14523f2757f'
