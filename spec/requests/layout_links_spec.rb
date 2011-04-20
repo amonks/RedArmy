0550e275a91817828c9dbfeeb8d7e94263271004
ajm <ajm@andrewmonks.net>
2011-04-20 01:53:57 -0400

    link tests

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -20,4 +20,15 @@ describe "LayoutLinks" do
       get '/signup'
       response.should have_selector('title', :content => "Sign up")
     end
+    it "should have the right links for navigation" do
+      visit root_path
+      click_link "About"
+      response.should have_selector 'title', :content => "About"
+      click_link "Contact"
+      response.should have_selector 'title', :content => "Contact"
+      click_link "Home"
+      response.should have_selector 'title', :content => "Home"
+      click_link "Enlist!"
+      response.should have_selector 'title', :content => "Sign up"
+    end
 end
